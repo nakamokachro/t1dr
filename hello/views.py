15fc6e803d95b7f2a4a9367dae43528d4f22dc09
nakamokachro <65682618+nakamokachro@users.noreply.github.com>
2020-07-31 17:38:13 -0500

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -10,4 +10,9 @@ def index(request):
 
 
 def db(request):
-    return render(request, "form.html")
+    if request.method == 'POST':
+        query = request.POST.get('textfield', None)
+        html = ("<H1>I will tl;dr this for you:</H1>" + query)
+        return HttpResponse(html)
+    else:
+        return render(request, 'form.html')
